4d7c6218fc93f2102ae6d55f99f44bc0c0ecd784
JUNAID <151039989+zunxii@users.noreply.github.com>
2025-05-23 18:50:32 +0530

    improved middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,27 @@
 import { withAuth } from "next-auth/middleware";
 
-export default withAuth({
-  pages: {
-    signIn: "/login", 
+export default withAuth(
+  function middleware(req) {
+    console.log("Middleware executing for:", req.nextUrl.pathname);
+    console.log("Token present:", !!req.nextauth.token);
   },
-});
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        if (token) return true;
+        if (req.nextUrl.pathname === "/") {
+          return false;
+        }
+        
+        return false;
+      },
+    },
+    pages: {
+      signIn: "/login",
+    },
+  }
+);
 
 export const config = {
-  matcher: ["/","/all-orders"], 
+  matcher: ["/", "/all-orders"],
 };
