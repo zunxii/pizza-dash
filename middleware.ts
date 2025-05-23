b019d2f3787f1741f6d3e651c2ad2e259f4e802a
JUNAID <151039989+zunxii@users.noreply.github.com>
2025-05-23 22:17:02 +0530

    debugging middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
-import { withAuth } from "next-auth/middleware";
+// import { withAuth } from "next-auth/middleware";
 
-export default withAuth(
-  function middleware(req) {
-    console.log("Middleware executing for:", req.nextUrl.pathname);
-    console.log("Token present:", !!req.nextauth.token);
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => {
-        return !!token; 
-      },
-    },
-    pages: {
-      signIn: "/login",
-    },
-  }
-);
+// export default withAuth(
+//   function middleware(req) {
+//     console.log("Middleware executing for:", req.nextUrl.pathname);
+//     console.log("Token present:", !!req.nextauth.token);
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => {
+//         return !!token; 
+//       },
+//     },
+//     pages: {
+//       signIn: "/login",
+//     },
+//   }
+// );
 
-export const config = {
-   matcher: ["/((?!login|_next|favicon.ico|images|api).*)"],
-};
+// export const config = {
+//    matcher: ["/((?!login|_next|favicon.ico|images|api).*)"],
+// };
